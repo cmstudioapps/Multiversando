7e9d70fd84a377c3d08473ca590b8e6e64de7d59
cmstudioapps <138110988+cmstudioapps@users.noreply.github.com>
2025-05-17 10:40:33 -0300

    Atualizar o post.js

--- api/post.js
+++ api/post.js
@@ -1,29 +1,27 @@
-export default function handler (req, res) {
+export default function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
-res.setHeader('Access-Control-Allow-Origin', '*');
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
 
-res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  const { dados } = req.body;
 
-const { dados } = req.body
+  if (!dados) {
+    return res.status(400).send("Dados vazios");
+  }
 
-if(!dados) {
-return res.status(400).send("Dados vazios")
-}
-
-
-fetch("https://cm-tube-default-rtdb.firebaseio.com/feed/.json", {
-
-method: "POST",
-headers: {
-"Content-Type":"application/json"
-},
-body: JSON.stringify(dados)
-
-}).then(response => response.json())
-.then( data => res.status(200).send("Enviado ao banco de dados com sucesso!"))
-.catch(error => res.status(400).send("Erro ao enviar ao banco de dados: "+error))
-
-
-
+  fetch("https://cm-tube-default-rtdb.firebaseio.com/feed/.json", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(dados)
+  })
+    .then(response => response.json())
+    .then(data => res.status(200).send("Enviado ao banco de dados com sucesso!"))
+    .catch(error => res.status(400).send("Erro ao enviar ao banco de dados: " + error));
 }
