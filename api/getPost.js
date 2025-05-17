c9da757f71a5de455bf3f136b970592164693a70
cmstudioapps <138110988+cmstudioapps@users.noreply.github.com>
2025-05-17 11:02:19 -0300

    Atualizar o getPost.js

--- api/getPost.js
+++ api/getPost.js
@@ -1,3 +1,17 @@
 export default function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
-}
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+
+  
+  if (!dados) {
+    return res.status(400).send("Dados vazios");
+  }
+
+  fetch("https://cm-tube-default-rtdb.firebaseio.com/feed/.json").then(response => response.json())
+.then(data => res.status(200).send(data)).catch (error => res.status(400).send(error))
