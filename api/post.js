9d0fdef3cdf7f3a44f6cbeefd2448394a0e3cff1
cmstudioapps <138110988+cmstudioapps@users.noreply.github.com>
2025-05-17 10:39:01 -0300

    Atualizar o post.js

--- api/post.js
+++ api/post.js
@@ -2,7 +2,7 @@ export default function handler (req, res) {
 
 res.setHeader('Access-Control-Allow-Origin', '*');
 
-res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
 res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
 const { dados } = req.body
