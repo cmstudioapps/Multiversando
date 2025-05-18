3e66c82d3dcaebbcd16aa8fe4928b8535d86ea9f
cmstudioapps <138110988+cmstudioapps@users.noreply.github.com>
2025-05-18 15:02:39 -0300

    Atualizar o post.js

--- api/post.js
+++ api/post.js
@@ -8,20 +8,44 @@ export default function handler(req, res) {
     return;
   }
 
-  const dados  = req.body;
+  const dados = req.body;
 
-  if (!dados) {
-    return res.status(400).send("Dados vazios");
+  if (!dados || !dados.nome || !dados.senha) {
+    return res.status(400).send("Dados vazios ou incompletos");
   }
 
-  fetch("https://cm-tube-default-rtdb.firebaseio.com/feed/.json", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(dados)
-  })
+  const login = {
+    nome: dados.nome,
+    senha: dados.senha
+  };
+
+  delete dados.nome;
+  delete dados.senha;
+
+  fetch("https://cm-tube-default-rtdb.firebaseio.com/adm/" + login.nome + ".json")
     .then(response => response.json())
-    .then(data => res.status(200).send("Enviado ao banco de dados com sucesso!"))
-    .catch(error => res.status(400).send("Erro ao enviar ao banco de dados: " + error));
+    .then(data => {
+      if (data && data.senha === login.senha) {
+        // Login válido, salva no banco de dados
+        fetch("https://cm-tube-default-rtdb.firebaseio.com/feed/.json", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(dados)
+        })
+          .then(response => response.json())
+          .then(() => {
+            res.status(200).json({ logar: true, mensagem: "Enviado ao banco de dados com sucesso!" });
+          })
+          .catch(error => {
+            res.status(500).json({ logar: true, erro: "Erro ao enviar ao banco de dados: " + error });
+          });
+      } else {
+        res.status(401).json({ logar: false, erro: "Senha ou nome incorretos" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ logar: false, erro: "Erro ao verificar login: " + error });
+    });
 }
