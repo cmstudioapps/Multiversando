40026f4b34f7620209b13d0dd4d099b38761eb49
cmstudioapps <138110988+cmstudioapps@users.noreply.github.com>
2025-05-18 15:01:17 -0300

    Atualizar o chatSend.js

--- api/chatSend.js
+++ api/chatSend.js
@@ -8,47 +8,20 @@ export default function handler(req, res) {
     return;
   }
 
-  const dados = req.body;
+  const dados  = req.body;
 
-  if (!dados || !dados.senha || !dados.nome) {
-    return res.status(400).send("Dados vazios ou incompletos");
+  if (!dados) {
+    return res.status(400).send("Dados vazios");
   }
 
-  const login = {
-    senha: dados.senha,
-    nome: dados.nome
-  };
-
-  delete dados.senha;
-  delete dados.nome;
-
-  fetch("https://cm-tube-default-rtdb.firebaseio.com/adm/" + login.nome + ".json")
+  fetch("https://cm-tube-default-rtdb.firebaseio.com/chat/.json", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(dados)
+  })
     .then(response => response.json())
-    .then(data => {
-      if (data && data.senha === login.senha) {
-        // Primeiro envia a mensagem ao banco, depois responde ao cliente
-        enviar(dados)
-          .then(() => {
-            res.status(200).json({ logar: true, mensagem: "Enviado ao banco de dados com sucesso!" });
-          })
-          .catch(error => {
-            res.status(500).json({ logar: true, erro: "Erro ao enviar ao banco de dados: " + error });
-          });
-      } else {
-        res.status(401).json({ logar: false, erro: "Senha ou nome incorretos" });
-      }
-    })
-    .catch(err => {
-      res.status(500).json({ logar: false, erro: "Erro ao verificar login" });
-    });
-
-  function enviar(dadosLimpos) {
-    return fetch("https://cm-tube-default-rtdb.firebaseio.com/chat/.json", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(dadosLimpos)
-    }).then(response => response.json());
-  }
+    .then(data => res.status(200).send("Enviado ao banco de dados com sucesso!"))
+    .catch(error => res.status(400).send("Erro ao enviar ao banco de dados: " + error));
 }
