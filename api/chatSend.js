b98ab5e7eb75352d597627ecd9829f673e00e520
cmstudioapps <138110988+cmstudioapps@users.noreply.github.com>
2025-05-17 21:42:02 -0300

    Atualizar o chatSend.js

--- api/chatSend.js
+++ api/chatSend.js
@@ -1,3 +1,27 @@
-export default function handler (req, res) {
+export default function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+
+  const dados  = req.body;
+
+  if (!dados) {
+    return res.status(400).send("Dados vazios");
+  }
+
+  fetch("https://cm-tube-default-rtdb.firebaseio.com/chat/.json", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(dados)
+  })
+    .then(response => response.json())
+    .then(data => res.status(200).send("Enviado ao banco de dados com sucesso!"))
+    .catch(error => res.status(400).send("Erro ao enviar ao banco de dados: " + error));
 }
