4e1f2c4bd7adea4d557b5ee216143472070fa129
cmstudioapps <138110988+cmstudioapps@users.noreply.github.com>
2025-05-22 05:47:36 -0300

    Atualizar o noti.js

--- api/noti.js
+++ api/noti.js
@@ -1,4 +1,8 @@
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
   if (req.method !== "GET") {
     return res.status(405).json({ error: "Método não permitido" });
   }
