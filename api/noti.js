e05da5bfa342484b35c055ac93343659c3d136ca
cmstudioapps <138110988+cmstudioapps@users.noreply.github.com>
2025-05-23 11:38:00 -0300

    Atualizar o noti.js

--- api/noti.js
+++ api/noti.js
@@ -17,8 +17,8 @@ export default async function handler(req, res) {
     const data = await response.json();
     res.status(200).json(data);
 
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Erro ao enviar notificação' });
-  }
+  } .catch(err => {
+  console.error("Erro:", err);
+  res.status(500).json({ error: err.message });
+});
 }
